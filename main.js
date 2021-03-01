e3a9bb1e55e0f0bf8e05a29f2535039acf339e27
Spencer Kekauoha <skekauoha@swipeclock.com>
2021-02-28 22:49:01 -0700

    setup base electron app

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log('hello world')
+const { app, BrowserWindow } = require("electron");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    backgroundColor: "white",
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  win.loadFile("index.html");
+}
+
+app.whenReady().then(createWindow);
