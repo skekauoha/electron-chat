46e87f189626c454bc5af5595993da41052304cd
Spencer Kekauoha <spkekauoha@gmail.com>
2021-03-23 22:41:16 -0600

    use HOC for baseLayout

--- src/js/layouts/Base.js
+++ src/js/layouts/Base.js
@@ -9,3 +9,20 @@ export default function BaseLayout({ children, ...props }) {
     </>
   );
 }
+
+function getDisplayName(Component) {
+  return Component.displayName || Component.name || 'Component';
+}
+
+// Higher order component is function that takes in a component and returns a new component
+// taking in Component and returns a new stateless functional component with props
+// Example: Chat.js -> export default withBaseLayout(Chat, { canGoBack: true });
+export const withBaseLayout = (Component, config) => (props) => {
+  const viewName = getDisplayName(Component);
+  return (
+    <>
+      <Navbar {...config} view={viewName} />
+      <Component {...props} />
+    </>
+  );
+};
--- src/js/views/Chat.js
+++ src/js/views/Chat.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import BaseLayout from '../layouts/Base';
+import { withBaseLayout } from '../layouts/Base';
 
 import ChatMessagesList from '../components/ChatMessagesList';
 import ChatUsersList from '../components/ChatUsersList';
@@ -10,18 +10,16 @@ const Chat = () => {
   const { id } = useParams();
 
   return (
-    <BaseLayout canGoBack>
-      <div className="row no-gutters fh">
-        <div className="col-3 fh">
-          <ChatUsersList />
-        </div>
-        <div className="col-9 fh">
-          <ViewTitle text={`Joined channel: ${id}`} />
-          <ChatMessagesList />
-        </div>
+    <div className="row no-gutters fh">
+      <div className="col-3 fh">
+        <ChatUsersList />
       </div>
-    </BaseLayout>
+      <div className="col-9 fh">
+        <ViewTitle text={`Joined channel: ${id}`} />
+        <ChatMessagesList />
+      </div>
+    </div>
   );
 };
 
-export default Chat;
+export default withBaseLayout(Chat, { canGoBack: true });
